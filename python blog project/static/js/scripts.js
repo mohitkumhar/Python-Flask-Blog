135cd9321cdc252a5f51d6baabb7953d19d163be
Mohit Kumhar <mohitmolela@gmail.com>
2023-06-16 19:57:31 +0530

    Update scripts.js

--- python blog project/static/js/scripts.js
+++ python blog project/static/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
-*/
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
